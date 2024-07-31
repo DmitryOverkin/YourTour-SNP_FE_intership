99401ee9a2348a8fd773601dda8328b9b0f2ceab
DmitryOverkin <overkindmitry@gmail.com>
2024-07-31 18:02:14 +0300

    Добавить функцилонал header(При скролле на 450px появляется шапка и имеет другие стили)

--- main.js
+++ main.js
@@ -4,13 +4,28 @@ const menuItemLinks = document.querySelectorAll(
   ".choose_your_tour__content-list-item-link"
 );
 
-menuItemLinks.forEach(item => {
-  item.addEventListener('click', function(event){
+menuItemLinks.forEach((item) => {
+  item.addEventListener("click", function (event) {
     event.preventDefault();
 
-    menuItemLinks.forEach(link => link.classList.remove('active'));
+    menuItemLinks.forEach((link) => link.classList.remove("active"));
 
-    item.classList.add('active')
-  })
-})
+    item.classList.add("active");
+  });
+});
 
+// Изменнение header при скролле
+
+const header = document.querySelector(".header");
+const logo = document.querySelector(".logo_img");
+console.log(logo);
+
+window.addEventListener("scroll", function () {
+  if (this.scrollY > 450) {
+    header.classList.add("scroll_header");
+    logo.classList.add("black");
+  } else {
+    header.classList.remove("scroll_header");
+    logo.classList.remove("black");
+  }
+});
